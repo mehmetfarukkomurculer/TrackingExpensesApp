00fa2d33d561987c3b9d9a858c7d8dbd78d0fa19
mehmetfarukkomurculer <mfkomurculer@gmail.com>
2024-01-12 02:49:11 +0300

    added todos to stats screen

--- screens/tabBararScreens/Stats.tsx
+++ screens/tabBararScreens/Stats.tsx
@@ -1,8 +1,10 @@
 import { View, Text } from "react-native";
+import Filter from "../../components/UI/Filter";
 
 const Stats = () => {
   return (
     <View>
+      
       <Text>Stats Screen</Text>
     </View>
   );
